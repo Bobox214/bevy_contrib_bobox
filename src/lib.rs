581d4d82e49ce4066f12330c3eba5938e9b4e3c1
Boris Boutillier <boris.boutillier@gmail.com>
2020-10-09 10:59:01 +0200

    Add RapierUtilsPlugin behind feature "rapier2d"

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
 mod cursor_2dworld_pos_plugin;
 pub use cursor_2dworld_pos_plugin::*;
+
+#[cfg(feature = "rapier2d")]
+mod rapier2d_utils_plugin;
+#[cfg(feature = "rapier2d")]
+pub use rapier2d_utils_plugin::*;
